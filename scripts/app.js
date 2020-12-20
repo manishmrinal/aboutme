7b0d125cb7edcfa224e7fcf18d18576d8cb06f9b
MANISH MRINAL <manishmrinalface@gmail.com>
2020-12-20 19:30:37 +0530

    added entry anmation

--- scripts/app.js
+++ scripts/app.js
@@ -44,5 +44,11 @@ const navSlide =() =>{
 
 }
 
+const t1=gsap.timeline({deafults:{ease: "power1.out"} } );
+t1.to('.text',{y:"0%",duration:1 , stagger:0.25});
+t1.to('.slider' , {y: "-100%",duration:1.5 , delay:0.5});
+t1.to('.intro',{y:"-100%", duraton:1} ,"-=1");
+t1.fromTo('nav',{opacity:0},{opacity: 1, duration:1});
+
 navSlide();
 
